b1f27b7d59d23781037a9ae7ce089c94a2149fd4
ABurro <andreaburro@icloud.com>
2024-12-06 10:37:57 +0100

    Fixing typos

--- 05_data_analysis/exercises/old/solutions_5.R
+++ 05_data_analysis/exercises/old/solutions_5.R
@@ -2,12 +2,15 @@
 #' Data Handling Exercise 5
 #' 
 #' Version 1: Aurélien Sallin, 02.12.2022
-#' - Update: 06.12.2024 (Andrea)
+#' - Update: 27.11.2023 (Andrea)
 #############################################################################
+
+# Set path
+path <- "05_data_analysis/data/"
 
 # Load packages using `pacman`
 require(pacman)
-p_load(dplyr, ggplot2, tidyr, readr)
+pacman::p_load(dplyr, ggplot2, tidyr, readr)
 
 
 # Exercise A ----------------------------------------------------------------
@@ -15,7 +18,7 @@ p_load(dplyr, ggplot2, tidyr, readr)
 # pivot_wider() to transform it into wide format (separate columns for different years). Store the resulting
 # tibble/data.frame in a variable called stocks2. Then, run the following code.
 
-stocks <- read_csv("stocks.csv")
+stocks <- read_csv(paste0(path, "stocks.csv"))
 
 dim(stocks)
 # This dataset has 4 rows and 3 columns
@@ -41,7 +44,7 @@ str(stocks)
 #' Read `countries.csv` into RStudio (store it in variable `countries`). 
 #' Then, run the following code.
 
-countries <- read_csv("countries.csv")
+countries <- read_csv(paste0(path, "countries.csv"))
 
 pivot_longer(countries, 1999, 2000, names_to = "year", values_to = "cases")
 
@@ -58,10 +61,10 @@ pivot_longer(countries, c("1999", "2000"), names_to = "year", values_to = "cases
 #' to fix the problem?
 
 # Read the data
-people <- read_csv("people.csv")
+people <- read_csv(paste0(path, "people.csv"))
 
 # Look at the dataset
-#str(people)
+str(people)
 
 # Spread the data ("spread" means pivot wider)
 pivot_wider(people, names_from = key, values_from =  value)
@@ -75,13 +78,13 @@ pivot_wider(people, names_from = key, values_from =  value)
 people$id <- c(1,1,2,3,3)
 pivot_wider(people, names_from = key, values_from = value)
 
-#' Or: remove the unnecessary raw
-people <- read_csv("people.csv")
+#' Or: remove the unnecessary column
+people <- read_csv("05_data_analysis/data/people.csv")
 people <- people[-3, ]
 pivot_wider(people, names_from = key, values_from = value)
 
-#' Or: Take the mean age and remove the unnecessary raw
-people <- read_csv("people.csv")
+#' Or: Take the mean age and remove the unnecessary column
+people <- read_csv(paste0(path, "people.csv"))
 people <- people %>%
     group_by(name, key) %>% # group data
     mutate(value = mean(value)) %>% # compute average
@@ -95,7 +98,7 @@ pivot_wider(people, names_from = key, values_from = value)
 #' What are the variables?
 
 # Sample solution
-preg <- read_csv("preg.csv")
+preg <- read_csv(paste0(path, "preg.csv"))
 
 # use common sense ('heuristics') to figure out what information are contained
 preg <- pivot_longer(preg, c("male", "female"), names_to = "gender", values_to = "count")
@@ -116,11 +119,10 @@ df <- cbind(df,
             "Covid_2021" = df$Covid_2020 + rnorm(4, 0, 0.1),
             "AntiCovidExpenses_2021" = df$AntiCovidExpenses_2020 + rnorm(4, 0, 100))
 
-df_long <- pivot_longer(df, 
-                        cols = -Country, 
+df_long <- pivot_longer(df, cols = 2:5, 
                         names_to = c(".value", "Year"),
+                        values_to = c("n"),
                         names_sep = "_")
-
 cor(df_long$Covid, df_long$AntiCovidExpenses)
 
 # End of script.
